a8a3ba047dbc17d182f77bef24cc0d909993634c
Benchekroun Hamza <hbenchekro@TC405-114-14.insa-lyon.fr>
2017-10-06 16:18:45 +0200

    IO

---
+++ functions.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE_MAX_TRAME  64
+#define INPUT_FILE "data.sujet"
+#define HEADER_FOOTER 2
+#define SIZE_MAXI TAILLE_MAX_TRAME+HEADER_FOOTER
+
+
+void read_frame()
+{
+    FILE *f=NULL;
+    
+    char buffer[SIZE_MAXI];
+    int i,ch ;
+
+    f=fopen(INPUT_FILE,"r");
+
+    if (f == NULL)
+    {
+        printf("NO INPUT FILE");
+        exit(EXIT_FAILURE);
+    }
+
+    else
+    {
+        while (!feof(f))
+        {
+            fgets(buffer,SIZE_MAXI,f);
+        }
+    }
+
+    fclose(f);
+
+    printf("%s\n",buffer);
+}
+
+
+
+
+    
