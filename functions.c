cf706bd3999d5ff0a1abf67bdb9d66340cd94504
Benchekroun Hamza <hbenchekro@TC405-114-14.insa-lyon.fr>
2017-10-06 17:10:21 +0200

    HEADER

--- functions.c
+++ functions.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "demultiplex.h"
+#include <stdbool.h>
 
 #define TAILLE_MAX_TRAME  64
 #define INPUT_FILE "data.sujet"
 #define HEADER_FOOTER 2
 #define SIZE_MAXI TAILLE_MAX_TRAME+HEADER_FOOTER
 
+static bool isOpened;
+static FILE* outputFiles[MAX_NB_FIC];
+static FILE* inputFile;
+static FILE* rejectFile;
 
-void read_frame()
+void readFrame()
 {
     FILE *f=NULL;
     
-    char buffer[SIZE_MAXI];
-    int i,ch ;
+    static char buffer[SIZE_MAXI];
 
     f=fopen(INPUT_FILE,"r");
 
@@ -35,6 +40,34 @@ void read_frame()
     printf("%s\n",buffer);
 }
 
+void writeFrame(outputData data,int index)
+{
+   openFiles();
+
+   for(int i = 0; i <= DATA_SIZE; i++)
+   {
+      fputc(outputFiles[index], data.data);
+   }
+   
+   fprintf(outputFiles[index],"%d",data.size);
+} 
+
+void openFiles()
+{
+    if(!isOpened)
+    {
+       inputFile=fopen(INPUT_FILE,"r+");
+       rejectFile=fopen(REJECT_FILE,"r+");
+
+        for(int i=0; i<MAX_NB_FIC; i++)
+        {
+            char buffer[16];
+            sprintf(buffer,"%s%d",OUTPUT_FILE,i);
+            outputFiles[i]=fopen(buffer,"r+");
+        }
+    }
+}
+  
 
 
 
---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void readFrame();
+void writeFrame(outputData data,int index);
+void rejectFrame(char* c);
+void openFiles();
+
+#endif
